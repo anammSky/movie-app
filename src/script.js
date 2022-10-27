3ff32c861d3861bf155022ddc6239af0e8f5554f
rash <rashdan.developer@gmail.com>
2022-10-27 16:10:17 +0100

    new branch

--- src/script.js
+++ src/script.js
@@ -1,37 +1,119 @@
 const tilesData = [
-  { name: "Trending", searchTerm: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`, id: "popular" },
-  { name: "Top Rated", searchTerm: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`, id: "top__rated" },
-  { name: 'Action', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=28`, id: 'action'},
-  { name: 'Adventure', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=12`, id: 'adventure'},
-  { name: 'Animation', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=16`, id: 'animation'},
-  { name: 'Comedy', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=35`, id: 'comedy'},
-  { name: 'Crime', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=80`, id: 'crime'},
-  { name: 'Documentary', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=99`, id: 'documentary'},
-  { name: 'Drama', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=18`, id: 'Drama'},
-  { name: 'Family', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10751`, id: 'family'},
-  { name: 'Fantasy', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=14`, id: 'fantasy'},
-  { name: 'History', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=36`, id: 'history'},
-  { name: 'Horror', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=27`, id: 'horror'},
-  { name: 'Music', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10402`, id: 'music'},
-  { name: 'Mystery', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=9648`, id: 'mystery'},
-  { name: 'Romance', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10749`, id: 'romance'},
-  { name: 'Science Fiction', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=878`, id: 'science__fiction'},
-  { name: 'TV Movie', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10770`, id: 'tv__movie'},
-  { name: 'Thriller', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=53`, id: 'thriller'},
-  { name: 'War', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10752`, id: 'war'},
-  { name: 'Western', searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=37`, id: 'western'}
+  {
+    name: "Trending",
+    searchTerm: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    id: "popular",
+  },
+  {
+    name: "Top Rated",
+    searchTerm: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+    id: "top__rated",
+  },
+  {
+    name: "Action",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+    id: "action",
+  },
+  {
+    name: "Adventure",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=12`,
+    id: "adventure",
+  },
+  {
+    name: "Animation",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=16`,
+    id: "animation",
+  },
+  {
+    name: "Comedy",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+    id: "comedy",
+  },
+  {
+    name: "Crime",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=80`,
+    id: "crime",
+  },
+  {
+    name: "Documentary",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=99`,
+    id: "documentary",
+  },
+  {
+    name: "Drama",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=18`,
+    id: "Drama",
+  },
+  {
+    name: "Family",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10751`,
+    id: "family",
+  },
+  {
+    name: "Fantasy",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=14`,
+    id: "fantasy",
+  },
+  {
+    name: "History",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=36`,
+    id: "history",
+  },
+  {
+    name: "Horror",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=27`,
+    id: "horror",
+  },
+  {
+    name: "Music",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10402`,
+    id: "music",
+  },
+  {
+    name: "Mystery",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    id: "mystery",
+  },
+  {
+    name: "Romance",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    id: "romance",
+  },
+  {
+    name: "Science Fiction",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=878`,
+    id: "science__fiction",
+  },
+  {
+    name: "TV Movie",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10770`,
+    id: "tv__movie",
+  },
+  {
+    name: "Thriller",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=53`,
+    id: "thriller",
+  },
+  {
+    name: "War",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=10752`,
+    id: "war",
+  },
+  {
+    name: "Western",
+    searchTerm: `discover/movie?api_key=${API_KEY}&with_genres=37`,
+    id: "western",
+  },
 ];
 
 //GENRE SECTION GRABBERS
 const main = document.querySelector("main");
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
 //GET MOVIE DATA FUNCTION
 async function getMovies(typeList, el) {
   try {
-    const response = await fetch(
-      `${API_BASE_URL}${typeList}`
-    );
+    const response = await fetch(`${API_BASE_URL}${typeList}`);
     const data = await response.json();
     createFrontPageCard(data, el);
   } catch (error) {
@@ -73,7 +155,7 @@ function createFrontPageCard(data, el) {
 
     for (let id of movieData[i].genre_ids) {
       const genreTag = document.createElement("p");
-      genreTag.classList.add('genre__list')
+      genreTag.classList.add("genre__list");
       genreTag.textContent = genreIds[id];
       textBottom.append(genreTag);
     }
@@ -107,39 +189,35 @@ for (let tile of tilesData) {
   getMovies(tile.searchTerm, sectionRow);
 }
 
+const genreSection = document.querySelector(".genre__section");
+const genreListButton = document.querySelector(".fi-sr-list");
 
-const genreSection = document.querySelector('.genre__section')
-const genreListButton = document.querySelector('.fi-sr-list')
+genreListButton.addEventListener("click", () => {
+  const genreAside = createElwithClass("aside", "sidebar");
 
-genreListButton.addEventListener('click', () => {
-  const genreAside = createElwithClass('aside', 'sidebar')
+  const genreTopDiv = createElwithClass("div", "sidebar__top");
+  const closeBtnImg = createElwithClass("img", "closeIcon");
+  closeBtnImg.src = "assets/close.png";
+  genreTopDiv.append(closeBtnImg);
 
-  const genreTopDiv = createElwithClass('div', 'sidebar__top')
-  const closeBtnImg = createElwithClass('img', 'closeIcon')
-  closeBtnImg.src = 'assets/close.png'
-  genreTopDiv.append(closeBtnImg)
+  const genreBottomDiv = createElwithClass("div", "sidebar__bottom");
+  const genreListContainer = document.createElement("ul");
 
-  const genreBottomDiv = createElwithClass('div', 'sidebar__bottom')
-  const genreListContainer = document.createElement('ul')
-  
-for (let genre of tilesData) {
-  const genreListItem = createElwithClass('li', 'genreItem')
-  const scrollToItem = document.createElement('a')
-  scrollToItem.href = `#${genre.id}`
-  scrollToItem.textContent = genre.name
-  genreListItem.append(scrollToItem)
-  genreListContainer.append(genreListItem)
-}
+  for (let genre of tilesData) {
+    const genreListItem = createElwithClass("li", "genreItem");
+    const scrollToItem = document.createElement("a");
+    scrollToItem.href = `#${genre.id}`;
+    scrollToItem.textContent = genre.name;
+    genreListItem.append(scrollToItem);
+    genreListContainer.append(genreListItem);
+  }
 
-genreBottomDiv.append(genreListContainer)
-genreAside.append(genreTopDiv, genreBottomDiv)
+  genreBottomDiv.append(genreListContainer);
+  genreAside.append(genreTopDiv, genreBottomDiv);
 
-body.append(genreAside)
+  body.append(genreAside);
 
-closeBtnImg.addEventListener('click', () => {
-  genreAside.textContent = ''
-})
-
-})
-
-
+  closeBtnImg.addEventListener("click", () => {
+    genreAside.textContent = "";
+  });
+});
