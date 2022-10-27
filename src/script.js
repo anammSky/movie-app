a6d49148b5593fd609158b394365807308909d0a
Ana Moros <ana.morosmarmol@sky.uk>
2022-10-27 11:25:37 +0100

    cleaned up front page scripts

--- src/script.js
+++ src/script.js
@@ -1,143 +1,87 @@
-// SAMPLE CODE TO CHECK API CALL
-
-//const API_URL = "https://api.themoviedb.org/3/";
+const tilesData = [
+  { name: "Trending", searchTerm: "popular", id: "popular" },
+  { name: "Top Rated", searchTerm: "top_rated", id: "top__rated" },
+];
 
 //GENRE SECTION GRABBERS
-const popular = document.querySelector('#popular')
-const topRated = document.querySelector('#top__rated')
+const main = document.querySelector("main");
 
-//POPULAR MOVIES FUNCTION
-async function popularMovies() {
-  //FETCH POPULAR MOVIES
-  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-  const data = await response.json();
-  console.log(data)
-  
-  const popularMovies = data.results
-
-  for (let i = 0; i < 20; i++) {
-
-    //CARD SECTION CREATE
-    const card = document.createElement('div')
-    card.classList.add('section__card')
-
-
-    //IMAGE SECTION CREATE
-    const sectionImage = document.createElement('div')
-    sectionImage.classList.add('section__image')
-    const movieImage = document.createElement('img')
-    movieImage.src = API_IMG.base_url + API_IMG.poster_sizes[2] + popularMovies[i].poster_path
-    movieImage.alt = 'MOVIE IMAGE NEEDS INSERTING'
-
-    //APPEND IMAGE TO DIV
-    sectionImage.append(movieImage)
-
-    //TEXT SECTION CREATE
-    const sectionText = document.createElement('div')
-    sectionText.classList.add('section__text')
-
-    //TEXT TOP 
-    const textTop = document.createElement('div')
-    textTop.classList.add('text__top')
-
-    //MOVIE TITLE CREATE
-    const movieTitle = document.createElement('p')
-    movieTitle.textContent = popularMovies[i].title
-    //APPEND MOVIE TITLE TO TEXT TOP
-    textTop.append(movieTitle)
-
-    //TEXT BOTTOM CREATE
-    const textBottom = document.createElement('div')
-    textBottom.classList.add('text__bottom')
-    const genreTag = document.createElement('p')
-    genreTag.textContent = 'GENRE'
-    const movieRating = document.createElement('p')
-    movieRating.textContent = popularMovies[i].vote_average
-
-    //APPEND GENRE AND TAG TO TEXT BOTTOM
-    textBottom.append(genreTag, movieRating)
-
-    //APPEND TEXT TO TEXT SECTION
-    sectionText.append(textTop, textBottom)
-
-
-    //APPENDS THE IMAGE AND MOVIE DETAILS TO THE CARD
-    card.append(sectionImage, sectionText)
-
-    //APPENDS THE WHOLE CARD TO THE PAGE
-    popular.append(card)
+//GET MOVIE DATA FUNCTION
+async function getMovies(typeList, el) {
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}movie/${typeList}?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    const data = await response.json();
+    createFrontPageCard(data, el);
+  } catch (error) {
+    throw error;
   }
 }
 
+//CREATE CARD FUNCTION
+function createFrontPageCard(data, el) {
+  const movieData = data.results;
 
-
-
-
-// TOP RATED FUNCTION
-async function topRatedMovies() {
-  //FETCH TOP RATED MOVIES
-  //const randomPage = Math.floor(Math.random() * 500)
-  const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
-  const data = await response.json();
-  
-  
-  const topRatedMovies = data.results
-
-  for (let i = 0; i < 20; i++) {
-
+  for (let i = 0; i < 10; i++) {
     //CARD SECTION CREATE
-    const card = document.createElement('div')
-    card.classList.add('section__card')
-
+    const card = createElwithClass("div", "section__card");
 
     //IMAGE SECTION CREATE
-    const sectionImage = document.createElement('div')
-    sectionImage.classList.add('section__image')
-    const movieImage = document.createElement('img')
-    movieImage.src = API_IMG.base_url + API_IMG.poster_sizes[2] + topRatedMovies[i].poster_path
-    movieImage.alt = 'MOVIE IMAGE NEEDS INSERTING'
+    const sectionImage = createElwithClass("div", "section__image");
+    const movieImage = document.createElement("img");
+    movieImage.src =
+      API_IMG.base_url + API_IMG.poster_sizes[2] + movieData[i].poster_path;
 
     //APPEND IMAGE TO DIV
-    sectionImage.append(movieImage)
+    sectionImage.append(movieImage);
 
     //TEXT SECTION CREATE
-    const sectionText = document.createElement('div')
-    sectionText.classList.add('section__text')
+    const sectionText = createElwithClass("div", "section__text");
 
-    //TEXT TOP 
-    const textTop = document.createElement('div')
-    textTop.classList.add('text__top')
+    //TEXT TOP
+    const textTop = createElwithClass("div", "text__top");
 
     //MOVIE TITLE CREATE
-    const movieTitle = document.createElement('p')
-    movieTitle.textContent = topRatedMovies[i].title
+    const movieTitle = document.createElement("p");
+    movieTitle.textContent = movieData[i].title;
     //APPEND MOVIE TITLE TO TEXT TOP
-    textTop.append(movieTitle)
+    textTop.append(movieTitle);
 
     //TEXT BOTTOM CREATE
-    const textBottom = document.createElement('div')
-    textBottom.classList.add('text__bottom')
-    const genreTag = document.createElement('p')
-    genreTag.textContent = 'GENRE'
-    const movieRating = document.createElement('p')
-    movieRating.textContent = topRatedMovies[i].vote_average
+    const textBottom = createElwithClass("div", "text__bottom");
+
+    for (let id of movieData[i].genre_ids) {
+      const genreTag = document.createElement("p");
+      genreTag.textContent = genreIds[id];
+      textBottom.append(genreTag);
+    }
+    const movieRating = document.createElement("p");
+    movieRating.textContent = movieData[i].vote_average;
 
     //APPEND GENRE AND TAG TO TEXT BOTTOM
-    textBottom.append(genreTag, movieRating)
+    textBottom.append(movieRating);
 
     //APPEND TEXT TO TEXT SECTION
-    sectionText.append(textTop, textBottom)
-
+    sectionText.append(textTop, textBottom);
 
     //APPENDS THE IMAGE AND MOVIE DETAILS TO THE CARD
-    card.append(sectionImage, sectionText)
+    card.append(sectionImage, sectionText);
 
     //APPENDS THE WHOLE CARD TO THE PAGE
-    topRated.append(card)
+    el.append(card);
   }
 }
 
+for (let tile of tilesData) {
+  const tileEl = createElwithClass("section", "genre__section");
+  tileEl.id = tile.id;
+  const title = document.createElement("h2");
+  title.textContent = tile.name;
+  const sectionRow = createElwithClass("div", "section__row");
 
+  tileEl.append(title, sectionRow);
+  main.append(tileEl);
 
-popularMovies()
-topRatedMovies()
+  getMovies(tile.searchTerm, sectionRow);
+}
